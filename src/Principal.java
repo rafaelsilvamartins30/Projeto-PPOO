bdc528592a882977ab3c5ff5d98fe65997bd2859
DiovaniMangiajr <diovanimacieljr@gmail.com>
2025-11-20 23:45:06 -0300

    Remoção de código comentado na classe Principal.

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,6 @@
 public class Principal{
   public static void main(String[] args){
     Simulador simulador = new Simulador();
-    //simulator.runLongSimulation();
     simulador.simular(500);
   }
 }
